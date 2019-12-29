1fe4f9634b93761fc6666a679844510ad64ad1ed
tylermade <tylerk.ryu@gmail.com>
2019-12-29 15:06:48 +0900

    Adding CI tools

---
+++ propertycrawler/config.py
@@ -0,0 +1,20 @@
+TOKEN_URL = 'https://developers.onemap.sg/publicapi/publicsessionid'
+
+DISTRICT_URL = 'https://developers.onemap.sg/publicapi/propsvc/retrieve_prop' \
+'erty_locations_within_district'
+
+POSTAL_DISTRICTS = range(1, 28)
+
+SEARCH_CONFIG = {
+    'apartment': 'true',
+    'condo': 'true',
+    'executive_condo': 'true',
+    'executive_hdb': 'true',
+    'five_room_hdb': 'true',
+    'four_room_hdb': 'true',
+    'landed': 'true',
+    'multi_gen_hdb': 'true',
+    'one_room_hdb': 'true',
+    'three_room_hdb': 'true',
+    'two_room_hdb': 'true'
+}
